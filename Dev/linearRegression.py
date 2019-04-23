29c25da5225af6365e3c31f987ad87e9c7b3f4de
Tyler Smith <35615709+tms0050@users.noreply.github.com>
2019-04-22 19:53:41 -0500

    final commit. Best squared error at 0.0033

--- Dev/linearRegression.py
+++ Dev/linearRegression.py
@@ -7,11 +7,31 @@ import os
 import decisionTree as dt
 import random
 import math
+import time
 
-# # source = https://stackoverflow.com/questions/5595425/what-is-the-best-way-to-compare-floats-for-almost-equality-in-python/33024979
-# # this is a math.isclose function that is implemented in python3, but not python2. Bringing it here for use
-# def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
-#     return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+def bayesExtract():
+    dirPath = os.path.abspath(os.path.dirname(__file__))
+    bayesPath = os.path.join(dirPath, 'HEC_guess.txt')
+    bayes = open(bayesPath)
+    lines = bayes.readlines()
+    returnDict = dict()
+    fastaPath = os.path.join(dirPath, '..', '5970_6970_SP_19_PROJECT_5', 'fasta')
+    fileList = os.listdir(fastaPath)
+    colPosition = 0
+    rowPosition = 0
+    for fileName in fileList:
+        strValue = ""
+        fastaFile = open(os.path.join(fastaPath, fileName))
+        fastaLines = fastaFile.readlines()
+        lengthToSearch = len(fastaLines[len(fastaLines) - 1])
+        for _ in range(0, lengthToSearch):
+            if colPosition == len(lines[rowPosition]):
+                colPosition = 0
+                rowPosition += 1
+            strValue += lines[rowPosition][colPosition]
+            colPosition += 1
+        returnDict[fileName[:4]] = strValue
+    return returnDict
 
 def pssmExtract(filename):   
     dirPath = os.path.abspath(os.path.dirname(__file__))
@@ -59,12 +79,12 @@ def combineFeatures():
         featureDict[fileName] = featureList[:]
     return featureDict
     
-def gradientDescent(featureDict, currentWeights, currentStep, testTMVals, stepLastTime):
+def gradientDescent(featureDict, currentWeights, currentStep, testTMVals, timeSinceLastStep):
     dirPath = os.path.abspath(os.path.dirname(__file__))
     weightsToTest = currentWeights[:]
     pssmPath = os.path.join(dirPath, '..', '5970_6970_SP_19_PROJECT_5', 'pssm')
     fileList = os.listdir(pssmPath)
-    for _ in range(0, 2000):
+    for _ in range(0, 200):
         val1 = int(random.random() * 100)
         val2 = int(random.random() * 150)
         while(val1 == val2):
@@ -76,19 +96,20 @@ def gradientDescent(featureDict, currentWeights, currentStep, testTMVals, stepLa
             newWeights[i + 25] = featureDict[fileList[val2][:4]][i]
         newWeights[50] = getTMValue(fileList[val1], fileList[val2])
         for j in range(0, len(currentWeights) - 2):
-            if(currentWeights[j] == newWeights[j]):
-                slope = 1
-            else:
+            try:
                 slope = (weightsToTest[len(currentWeights)-1] - newWeights[len(newWeights)-1])/(weightsToTest[j] - newWeights[j])
+            except:
+                slope = 1
             weightsToTest[j] = weightsToTest[j] + currentStep*(slope - weightsToTest[j])
     newVal = getAvgSqError(featureDict, weightsToTest, testTMVals)
     oldVal = getAvgSqError(featureDict, currentWeights, testTMVals)
-    if (oldVal < newVal) & (stepLastTime):
-        return currentWeights, (currentStep/10), False
+    newTime = timeSinceLastStep + 1
+    if (oldVal < newVal) & (timeSinceLastStep > 200):
+        return currentWeights, (currentStep/10), 0
     elif newVal < oldVal:
-        return weightsToTest, currentStep, False
+        return weightsToTest, currentStep, 0
     else:
-        return currentWeights, currentStep, True
+        return currentWeights, currentStep, newTime
 
 def getTMValue(protein1, protein2):
     tmalign = os.path.abspath(os.path.join(os.path.abspath(__file__), '..', '..', '5970_6970_SP_19_PROJECT_5', 'tmalign', protein1[:4] + '_' + protein2[:4] + '_tmalign'))
@@ -106,39 +127,54 @@ def getAvgSqError(featureDict, weightList, testTMVals):
         first25 = featureDict[key[:4]]
         second25 = featureDict[key[4:]]
         predTMVal = 0.0
-        for i in range(0, 25):
+        for i in range(0, 22):
             predTMVal += first25[i] * weightList[i]
-        for j in range(0, 25):
+        for j in range(0, 22):
             predTMVal += second25[j] * weightList[j + 25]
         totSqError += math.pow(correctAnswer - predTMVal, 2)
         counter += 1
     return totSqError/counter
     
-def getTestTMAlignVals():
+def getTMAlignVals(test=True):
     retDict = dict()
     dirPath = os.path.abspath(os.path.dirname(__file__))
     pssmPath = os.path.join(dirPath, '..', '5970_6970_SP_19_PROJECT_5', 'pssm')
     fileList = os.listdir(pssmPath)
-    for i in range(100, 150):
-        for j in range(0, 150):
-            if i==j:
-                continue
-            alignVal = getTMValue(fileList[i][:4], fileList[j][:4])
-            keyName = fileList[i][:4] + '' + fileList[j][:4]
-            retDict[keyName] = alignVal
+    if test:
+        for i in range(100, 150):
+            for j in range(0, 150):
+                if i==j:
+                    continue
+                alignVal = getTMValue(fileList[i][:4], fileList[j][:4])
+                keyName = fileList[i][:4] + '' + fileList[j][:4]
+                retDict[keyName] = alignVal
+    else:
+        for i in range(0, 100):
+            for j in range(0, 150):
+                if i==j:
+                    continue
+                alignVal = getTMValue(fileList[i][:4], fileList[j][:4])
+                keyName = fileList[i][:4] + '' + fileList[j][:4]
+                retDict[keyName] = alignVal
     return retDict
 
 def implementLinearRegression():
     myfeatures = combineFeatures()
     currentWeights = [0.0] * 51
     currentStep = 1.0
-    testTMVals = getTestTMAlignVals()
-    stepLastTime = True
-    while currentStep > 0.0000000000001:
-        currentWeights, currentStep, stepLastTime = gradientDescent(myfeatures, currentWeights, currentStep, testTMVals, stepLastTime)
-        if(stepLastTime):
-            print currentWeights
-            print getAvgSqError(myfeatures, currentWeights, testTMVals)
+    trainTMVals = getTMAlignVals(False)
+    testTMVals = getTMAlignVals()
+    timeSinceLastStep = 0
+    while currentStep > 0.00000001:
+        currentWeights, currentStep, timeSinceLastStep = gradientDescent(myfeatures, currentWeights, currentStep, trainTMVals, timeSinceLastStep)
+        if(timeSinceLastStep == 0):
+            print 'Current Weights: ', currentWeights
+            print 'Current Step Size: ', currentStep
+            print 'Current Avg Square Error on Train Data: ', getAvgSqError(myfeatures, currentWeights, trainTMVals)
+            print 'Current Avg Square Error on Test Data: ', getAvgSqError(myfeatures, currentWeights, testTMVals), '\n'
     
 if __name__ == '__main__':
+    start = time.time()
     implementLinearRegression()
+    end = time.time()
+    print('Took %d seconds to run' % (end - start))
